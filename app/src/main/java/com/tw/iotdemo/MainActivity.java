47ce37708872ee4a9909d5c44e39b9efd7988d10
Sumit Jadhav <summit.jadhav@gmail.com>
2019-03-08 11:18:28 +0530

    Add google sign in, and display user name when in range of beacon.

--- app/src/main/java/com/tw/iotdemo/MainActivity.java
+++ app/src/main/java/com/tw/iotdemo/MainActivity.java
@@ -1,5 +1,6 @@
 package com.tw.iotdemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,6 +14,12 @@ import com.estimote.proximity_sdk.api.ProximityObserverBuilder;
 import com.estimote.proximity_sdk.api.ProximityZone;
 import com.estimote.proximity_sdk.api.ProximityZoneBuilder;
 import com.estimote.proximity_sdk.api.ProximityZoneContext;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 import java.util.List;
 
@@ -23,10 +30,16 @@ import kotlin.jvm.functions.Function1;
 
 public class MainActivity extends AppCompatActivity {
     private ProximityObserver proximityObserver;
+    private GoogleSignInClient mGoogleSignInClient;
+    private Integer RC_SIGN_IN = 1;
+    private GoogleSignInAccount loggedInAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        createSignInActivity();
+
         setContentView(R.layout.activity_main);
 
         EstimoteCloudCredentials cloudCredentials =
@@ -51,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public Unit invoke(ProximityZoneContext context) {
                         String deskOwner = context.getAttachments().get("owner");
-                        String msg = "Welcome to " + deskOwner + "'s desk";
+                        String msg = "Welcome "+ loggedInAccount.getGivenName() + " to " + deskOwner + "'s desk";
                         Log.d("app", msg);
                         Toasty.info(getApplicationContext(), msg, Toast.LENGTH_LONG, true).show();
                         return null;
@@ -60,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
                 .onExit(new Function1<ProximityZoneContext, Unit>() {
                     @Override
                     public Unit invoke(ProximityZoneContext context) {
-                        String msg = "Bye bye, come again!";
+                        String msg = "Bye bye " + loggedInAccount.getGivenName() + ", come again!";
                         Log.d("app", msg);
                         Toasty.info(getApplicationContext(), msg, Toast.LENGTH_LONG, true).show();
                         return null;
@@ -95,5 +108,66 @@ public class MainActivity extends AppCompatActivity {
                         });
     }
 
-    
+    private void createSignInActivity() {
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        // Check for existing Google Sign In account, if the user is already signed in
+        // the GoogleSignInAccount will be non-null.
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        updateUI();
+        if (account == null)
+            signIn();
+        else {
+            loggedInAccount = account;
+            updateUI();
+        }
+
+    }
+
+    private void updateUI() {
+        if (loggedInAccount == null)
+            return;
+        Log.d("app", loggedInAccount.getGivenName());
+        Toasty.success(getApplicationContext(), "Logged in as " + loggedInAccount.getGivenName(), Toast.LENGTH_LONG, true);
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            loggedInAccount = account;
+            // Signed in successfully, show authenticated UI.
+            updateUI();
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w("app", "signInResult:failed code=" + e.getStatusCode());
+            updateUI();
+        }
+    }
+
+
 }
